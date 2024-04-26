88c8ee5504b0d8463e77f866af7a7cbfb6b07a16
Sarah Gallardo <gallardosarahgin2023@gmail.com>
2024-04-26 23:01:26 +0800

    pages

--- src/components/servicePage/ServicePage.jsx
+++ src/components/servicePage/ServicePage.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import AOS from "aos";
+import { PacmanLoader } from "react-spinners";
 import "aos/dist/aos.css";
 import "./Services.scss";
+
 const ServicePage = () => {
   useEffect(() => {
     const handleScroll = () => {
@@ -17,14 +19,14 @@ const ServicePage = () => {
   }, []);
 
   const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("/servicePage");
-        console.log("Service data:", response.data); // Log the response data
+        console.log("Service data:", response.data);
 
-        // Prepend base URL to relative image paths
         const servicesWithCompleteUrls = response.data.map((service) => ({
           ...service,
           image1: `${window.location.origin}/${service.image1}`,
@@ -33,6 +35,7 @@ const ServicePage = () => {
         }));
 
         setServices(servicesWithCompleteUrls);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -43,47 +46,48 @@ const ServicePage = () => {
 
   return (
     <section className="services" id="pricing">
-      <h2 data-aos="zoom-in-up">Services</h2>
+      {/* Dynamically display the title from service data */}
+      <h2 data-aos="zoom-in-up">{services.length > 0 && services[0].title1}</h2>
       <div className="services_container wrapper">
-        {services.map((service) => (
-          <>
-            <div className="services_card" key={service.id} data-aos="fade-up">
-              {/* Displaying Image 1 */}
-              <img src={service.image1} alt="" />
-              <h3>{service.title2}</h3>
-
-              <a href="/register" className="btn">
-                BOOK NOW
-              </a>
-            </div>
-            <div
-              className="services_card"
-              key={`${service.id}_2`}
-              data-aos="fade-up"
-            >
-              {/* Displaying Image 2 */}
-              <img src={service.image2} alt="" />
-              <h3>{service.title3}</h3>
-
-              <a href="/register" className="btn">
-                BOOK NOW
-              </a>
-            </div>
-            <div
-              className="services_card"
-              key={`${service.id}_3`}
-              data-aos="fade-up"
-            >
-              {/* Displaying Image 3 */}
-              <img src={service.image3} alt="" />
-              <h3>{service.title4}</h3>
-
-              <a href="/register" className="btn">
-                BOOK NOW
-              </a>
-            </div>
-          </>
-        ))}
+        {loading ? (
+          <div className="spinner-container">
+            <PacmanLoader color="#34c4a9" loading={loading} size={50} />
+          </div>
+        ) : (
+          services.map((service) => (
+            <>
+              <div className="services_card" key={service.id} data-aos="fade-up">
+                <img src={service.image1} alt="" />
+                <h3>{service.title2}</h3>
+                <a href="/register" className="btn">
+                  BOOK NOW
+                </a>
+              </div>
+              <div
+                className="services_card"
+                key={`${service.id}_2`}
+                data-aos="fade-up"
+              >
+                <img src={service.image2} alt="" />
+                <h3>{service.title3}</h3>
+                <a href="/register" className="btn">
+                  BOOK NOW
+                </a>
+              </div>
+              <div
+                className="services_card"
+                key={`${service.id}_3`}
+                data-aos="fade-up"
+              >
+                <img src={service.image3} alt="" />
+                <h3>{service.title4}</h3>
+                <a href="/register" className="btn">
+                  BOOK NOW
+                </a>
+              </div>
+            </>
+          ))
+        )}
       </div>
     </section>
   );
