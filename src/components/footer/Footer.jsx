b171eac7c6662b4770315a85cde8c1d50c6b01af
Sarah Gallardo <gallardosarahgin2023@gmail.com>
2024-04-27 16:27:08 +0800

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,67 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import './Footer.css'; // You can create a separate CSS file for styling
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaHome, FaUserMd , FaMobileAlt, FaEnvelope, FaPhone } from 'react-icons/fa';
+import './Footer.css';
 
-const Footer = () => {
+function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <div className="social-links">
-          {/* Add your social links with icons here */}
-          <a href="https://www.facebook.com/your-facebook-page" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} /></a>
-          <a href="https://www.twitter.com/your-twitter-page" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
-          <a href="https://www.instagram.com/your-instagram-page" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
-          {/* Add more social links as needed */}
+    <footer className="text-white text-center text-lg-start  martop"> 
+      <div className="row mt-4">
+        <div className="col-lg-4 col-md-12 mb-4 mb-md-0" style={{ paddingLeft: '30px' }}>
+          <h5 className="text-uppercase mb-4">About company</h5>
+          <p style={{ color: 'white' }}>
+            We’re on a mission to strengthen the human-animal bond through better pet care, and we started with a new approach: urgent care.
+          </p>
+          <p style={{ color: 'white' }}>
+            Our medical team, for whom a broad case mix provides opportunities to learn, grow and practice at the top of their license. Diverse cases keep the team engaged and deepens their passion for veterinary medicine.
+          </p>
         </div>
-        <div className="copyright">
-          <p>COPYRIGHT &copy; 2024. ALL RIGHTS RESERVED.</p>
+
+        <div className="col-lg-4 col-md-12 mb-4 mb-md-0" style={{ paddingLeft: '100px' }}>
+        <h5 className="text-uppercase mb-4">Contact</h5>
+          <div className="form-outline form-white mb-4"></div>
+          <ul className="fa-ul" style={{ marginLeft: '1.65em', color: 'white' }}>
+            <li className="mb-3">
+              <span className="fa-li"><FaUserMd/></span><span className="ms-2"> Dr. John Doe</span>
+            </li>
+            <li className="mb-3">
+              <span className="fa-li"><FaHome /></span><span className="ms-2"> 1st Floor, ABC Building, Purok 8, Taguig City</span>
+            </li>
+            <li className="mb-3">
+              <span className="fa-li"><FaEnvelope /></span><span className="ms-2">petcareest.20224@gmail.com</span>
+            </li>
+            <li className="mb-3">
+              <span className="fa-li"><FaPhone /></span><span className="ms-2">+ 654456456</span>
+            </li>
+            <li className="mb-3">
+              <span className="fa-li"><FaMobileAlt /></span><span className="ms-2">+ 091 234 567 89</span>
+            </li>
+          </ul>
         </div>
+
+        <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+          <h5 className="text-uppercase mb-4">Opening hours</h5>
+          <table className="table text-center text-white">
+            <tbody className="font-weight-normal">
+              <tr>
+                <td>Mon - Fri:</td>
+                <td>8am - 5pm</td>
+              </tr>
+            </tbody>
+          </table>
+          <h5 className="text-uppercase mb-4">Social Links</h5>
+          <div className="mt-4">
+            <a href="https://www.facebook.com/" target="_blank" className="btn btn-floating btn-primary btn-lg"><FaFacebookF /></a>
+            <a href="https://twitter.com/" target="_blank" className="btn btn-floating btn-primary btn-lg"><FaTwitter /></a>
+            <a href="https://instagram.com/" target="_blank" className="btn btn-floating btn-primary btn-lg"><FaInstagram /></a>
+          </div>
+        </div>          
+      </div>
+      <div className="text-center p-3" style={{ backgroundColor: '#226c97', color: 'white' }}>
+        Copyright © 2024 PetCare.All Rights Reserved.
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
